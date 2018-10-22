99eca975f0508730544d0344bcf76b9f85935fff
aplet123 <jasonqan2004@gmail.com>
2018-10-22 15:10:07 -0400

    juicy options

--- ui.js
+++ ui.js
@@ -2,3 +2,12 @@ var anscroll = document.getElementById("anscroll");
 anscroll.addEventListener("click", function () {
 	document.getElementById("answers").scrollIntoView();
 });
+var opBtn = document.getElementById("opbtn");
+var opbox = document.getElementById("options");
+opbtn.addEventListener("click", function () {
+	if (opbox.className == "hidden") {
+		opbox.className = "optionbox";
+	} else {
+		opbox.className = "hidden";
+	}
+});
